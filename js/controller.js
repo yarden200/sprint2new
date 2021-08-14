036ead21effb88824c32a02bd0e30fbb4b66e8d9
Yarden Shaham <60040078+yarden200@users.noreply.github.com>
2021-08-14 20:03:58 +0300

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -2,15 +2,124 @@
 
 var gElCanvas;
 var gCtx;
+var gElImg;
 
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
-
-    drawImg('2');
-    drawText('Hello',10,50);
-
+    renderGallery()
 }
+
+function renderGallery() {
+    var strHTML = ``
+    gImgs.forEach((img) => {
+        strHTML += `<img src="${img.url}" class="gallery-img" onclick="onOpenEditor(this,${img.id})">`
+    })
+
+    document.querySelector('.gallery').innerHTML = strHTML;
+}
+
+function onOpenEditor(elImg, imgId) {
+    setGMeme(imgId);
+    gElImg = elImg;
+    renderCanvas()
+    document.body.classList.toggle('editor-open');
+}
+
+function onCloseEditor(){
+    document.body.classList.toggle('editor-open');
+}
+
+
+
+
+// CANVAS //
+
+function renderCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+    drawImgByElImg(gElImg);
+    renderLines()
+}
+
+function renderLines() {
+    gMeme.lines.forEach((line) => drawTextLine(line))
+}
+
+function drawImgByElImg(elImg) {
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function drawTextLine(line) {
+    if (!line.txt) return
+    gCtx.font = `${line.size}px ${line.font}`
+    gCtx.fillStyle = `${line.color}`
+    gCtx.strokeStyle='black'
+    gCtx.fillText(line.txt, line.posX, line.posY)
+    gCtx.strokeText(line.txt, line.posX, line.posY)
+    if (line.isActive) drawLineBorder(line)
+}
+
+function drawLineBorder(line) {
+    var lineLen = gCtx.measureText(line.txt).width
+    gCtx.strokeStyle = 'grey'
+    gCtx.beginPath()
+    gCtx.rect(line.posX - 5, line.posY - line.size, lineLen + 10, line.size + 5)
+    gCtx.stroke()
+}
+
+// EDITOR BTNS //
+
+function onAddText() {
+    var txt = document.querySelector('[name=textLine]').value
+    if (txt === '') return
+    addTxt(txt)
+    renderCanvas()
+}
+
+function onChangeFontSize(diff) {
+    changeGMemeFontSize(diff)
+    renderCanvas()
+}
+
+function onMoveLine(diff) {
+    moveLine(diff)
+    renderCanvas()
+}
+
+function onSwitchLine() {
+    switchLine()
+    renderCanvas()
+}
+
+function onPickColor(color) {
+    changeColor(color)
+    renderCanvas()
+}
+
+function onTextAlign(dir) {
+    textAlign(dir)
+    renderCanvas()
+}
+
+
+function onChangeFont(font) {
+    changeFont(font)
+    renderCanvas()
+}
+
+
+function onDeleteLine() {
+    deleteLine()
+    renderCanvas()
+}
+
+function onAddLine() {
+    addLine()
+    renderCanvas()
+}
+
+
+
 
 
 
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,49 +1,125 @@
 'use strict'
 
 var gImgs = [
-    { id: '2', url: 'imgs/2.jpg', keywords: ['happy'] }
+    { id: '2', url: 'imgs/2.jpg', keywords: ['happy'] },
+    { id: '10', url: 'imgs/10.jpg', keywords: ['happy'] }
 ];
 
-
 var gMeme = {
-    selectedImgId: '2',
+    selectedImgId: null,
     selectedLineIdx: 0,
     lines: [
         {
+            isActive: true,
+            posX: 10,
+            posY: 100,
             txt: 'I never eat Falafel',
             size: 20,
-            align: 'left',
-            color: 'red'
+            color: 'white',
+            font: 'IMPACT'
         },
+        {
+            isActive: false,
+            posX: 10,
+            posY: 400,
+            txt: 'Canvas HTML',
+            size: 20,
+            color: 'white',
+            font: 'IMPACT'
+        }
     ]
 }
 
-function drawImg(imgId) {
-    var imgUrl=getImgUrlById(imgId)
-    var img = new Image()
-    img.src = imgUrl
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+
+function setGMeme(imgId) {
+    gMeme.selectedImgId = imgId;
+    gMeme.selectedLineIdx = 0;
+}
+
+function getGMemeCurrLine() {
+    return gMeme.lines[gMeme.selectedLineIdx];
+}
+
+function changeGMemeFontSize(diff) {
+    var line = getGMemeCurrLine()
+    if (line.size <= 8 && diff < 0 || line.size >= 100 && diff > 0) return
+    else line.size += diff;
+}
+
+function moveLine(diff) {
+    var line = getGMemeCurrLine()
+    if (line.posY === 0 && diff < 0 || line.posY === gElCanvas.height && diff > 0) return
+    else line.posY += diff
+}
+
+function addTxt(txt) {
+    var line = getGMemeCurrLine()
+    line.txt = txt
+}
+
+function switchLine() {
+    var line = getGMemeCurrLine()
+    line.isActive = false
+    var linseLen = gMeme.lines.length
+    if (gMeme.selectedLineIdx + 1 === linseLen) gMeme.selectedLineIdx = 0
+    else gMeme.selectedLineIdx++
+    line = getGMemeCurrLine()
+    line.isActive = true
+}
+
+function changeColor(color) {
+    var line = getGMemeCurrLine()
+    line.color = color
+}
+
+function textAlign(dir) {
+    var line = getGMemeCurrLine()
+    switch (dir) {
+        case 'left':
+            line.posX = 10
+            break;
+        case 'center':
+            line.posX = 175
+            break;
+        case 'right':
+            line.posX = 350
+            break;
+        default:
+            break;
     }
 }
 
-function drawText(txt, x, y) {
-    debugger
-    gCtx.font = '48px IMPACT';
-    gCtx.fillStyle = 'black';
-    gCtx.fillText(txt, x, y);
+function changeFont(font){
+    var line = getGMemeCurrLine()
+    line.font = font
 }
 
-function getImgUrlById(imgId) {
-    var img = getImgById(imgId);
-    return img.url
+function deleteLine() {
+    var line = getGMemeCurrLine()
+    line.txt = null
 }
 
-function getImgById(imgId) {
-    var img = gImgs.find(function (img) {
-        return imgId === img.id
+function addLine() {
+    var line = getGMemeCurrLine()
+    line.isActive = false
+    var newLine = createNewLine()
+    gMeme.lines.push(newLine)
+    var idx = gMeme.lines.findIndex((line) => {
+        return line.isActive === true
     })
-    return img
+    gMeme.selectedLineIdx=idx
+}
+
+function createNewLine() {
+    return {
+        isActive: true,
+        posX: 10,
+        posY: 245,
+        txt: 'New Line',
+        size: 20,
+        color: 'white',
+        font: 'IMPACT'
+    }
 }
 
 
